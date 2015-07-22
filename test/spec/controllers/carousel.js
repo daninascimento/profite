1f79979f11a50eb21ab904a03c8e2c5b81fffd53
Daniel Lima <daniellima.web@gmail.com>
2015-07-22 16:57:44 -0300

    ajustes menores e deleção de arquivos

--- test/spec/controllers/carousel.js
+++ test/spec/controllers/carousel.js
@@ -16,8 +16,4 @@ describe('Controller: CarouselCtrl', function () {
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(CarouselCtrl.awesomeThings.length).toBe(3);
-  });
 });
--- test/spec/controllers/products.js
+++ test/spec/controllers/products.js
@@ -16,8 +16,4 @@ describe('Controller: ProductsCtrl', function () {
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(ProductsCtrl.awesomeThings.length).toBe(3);
-  });
 });
